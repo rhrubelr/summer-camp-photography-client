d4917ad8569ae91469f3c9baafda043fb8e9977e
rhrubelr <rhrubel246@gmail.com>
2023-06-07 02:12:11 +0600

    setup router

---
+++ src/Router/Router.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Main from '../Layout/Main';
+import { createBrowserRouter } from 'react-router-dom';
+import Home from '../Pages/Home/Home';
+
+
+export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        },
+      ]
+    },
+  ]);
+
+
+
+
