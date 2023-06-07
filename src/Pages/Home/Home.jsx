2266c8e1fad068db9d9cf1588d1376eb72703b09
rhrubelr <rhrubel246@gmail.com>
2023-06-07 14:15:53 +0600

    added error page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
-import Carusel from '../Carusel/Carusel';
 
 const Home = () => {
     return (
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Main from '../Layout/Main';
 import { createBrowserRouter } from 'react-router-dom';
 import Home from '../Pages/Home/Home';
+import ErrorPage from '../Pages/ErrorPage/ErrorPage';
 
 
 export const router = createBrowserRouter([
     {
       path: "/",
       element: <Main></Main>,
+      errorElement: <ErrorPage></ErrorPage>,
       children: [
         {
           path: '/',
