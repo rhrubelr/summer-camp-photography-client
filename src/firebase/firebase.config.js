83471d8984a7677cc7d4ed8a02b82c3a09ea9caf
rhrubelr <rhrubel246@gmail.com>
2023-06-08 01:01:47 +0600

    added authProver

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -14,20 +14,4 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyB39xKtuaph3-NNIyYol8jIxJBvJV0p3Z4",
-  authDomain: "photography-school-b3679.firebaseapp.com",
-  projectId: "photography-school-b3679",
-  storageBucket: "photography-school-b3679.appspot.com",
-  messagingSenderId: "39115198986",
-  appId: "1:39115198986:web:f6f8e21af74930e09545f3"
-};
-
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,15 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import { router } from './Router/Router';
+import AuthProvider from './Provider/AuthProvider';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div className='max-w-screen-xl mx-auto'>
-      <RouterProvider router={router} />
-    </div>
+    <AuthProvider>
+      <div className='max-w-screen-xl mx-auto'>
+        <RouterProvider router={router} />
+      </div>
+    </AuthProvider>
   </React.StrictMode>,
 )
