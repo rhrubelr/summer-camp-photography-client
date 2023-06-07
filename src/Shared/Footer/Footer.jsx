680b466e82f648733082a92cae0afe83e9bd41c3
rhrubelr <rhrubel246@gmail.com>
2023-06-07 11:31:05 +0600

    added footer

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -3,7 +3,45 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <h2>footer</h2>
+            <footer className="footer p-10 bg-teal-800 text-base-content text-white">
+                <div>
+                    <span className="footer-title">Services</span>
+                    <a className="link link-hover">Branding</a>
+                    <a className="link link-hover">Design</a>
+                    <a className="link link-hover">Marketing</a>
+
+                </div>
+                <div>
+                    <span className="footer-title">Company</span>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Contact</a>
+                    <a className="link link-hover">Jobs</a>
+
+                </div>
+                <div>
+                    <span className="footer-title">Legal</span>
+                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+
+                </div>
+                <div>
+                    <span className="footer-title">Newsletter</span>
+                    <div className="form-control w-80">
+                        <label className="label">
+                            <span className="label-text text-white">Enter your email address</span>
+                        </label>
+                        <div className="relative">
+                            <input type="text" placeholder="username@site.com" className="input input-bordered w-full pr-16" />
+                            <button className="btn btn-primary absolute top-0 right-0 rounded-l-none">Subscribe</button>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+            <footer className="footer footer-center p-4 bg-teal-800 text-base-content ">
+                <div>
+                    <p className='text-white'>Copyright © 2023 - All right reserved by Photography Industries Ltd</p>
+                </div>
+            </footer>
         </div>
     );
 };
--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -6,26 +6,28 @@ const Navbar = () => {
         <li><Link to='/'><a>Home</a></Link></li>
         <li><Link to='/instructor'><a>Instructor</a></Link></li>
         <li><Link to='/classes'><a>Classes</a></Link></li>
-        
+
     </>
     return (
         <div>
+            
+
             <div className="navbar bg-teal-700 text-white">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-teal-900 rounded-box w-52">
-                             {NavItems}
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                            {NavItems}
                         </ul>
                     </div>
-                    <img className='w-10 h-10 cursor-pointer'  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDg0Zfjx1LSbn8-wBGoM4MhBD5DIM6WdzjoVqxKobG2HUDCg6yrKkB9R94B5SWLaZi1F0&usqp=CAU" alt="" />
-                    <a className="btn btn-ghost normal-case text-xl text-white">Digital Photography School</a>
+                    <img className='w-8 h-8 sm:w-10 sm:h-10 cursor-pointer'  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDg0Zfjx1LSbn8-wBGoM4MhBD5DIM6WdzjoVqxKobG2HUDCg6yrKkB9R94B5SWLaZi1F0&usqp=CAU" alt="" />
+                    <a className="btn btn-ghost normal-case text-xl"> Photography School</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                    {NavItems}
+                        {NavItems}
                     </ul>
                 </div>
                 <div className="navbar-end">
