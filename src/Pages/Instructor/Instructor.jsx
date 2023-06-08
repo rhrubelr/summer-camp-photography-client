88f1c8cec9b7c239197e6f4292ab73749593a69d
rhrubelr <rhrubel246@gmail.com>
2023-06-09 03:41:47 +0600

    added instructor card

--- src/Pages/Instructor/Instructor.jsx
+++ src/Pages/Instructor/Instructor.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import InstructorCart from './InstructorCart';
+
 
 const Instructor = () => {
+    const [populars, setPopulars] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:5000/instructor")
+            .then(res => res.json())
+            .then(data => {
+                setPopulars(data)
+            })
+    }, [])
     return (
         <div>
-            
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-10 mx-auto my-10'>
+                {
+                    populars.map(popular =>
+                        <InstructorCart
+                            key={popular._id}
+                            popular={popular}
+                        ></InstructorCart>)
+                }
+            </div>
         </div>
     );
 };
--- src/Pages/PopularInstructor/PopularInstructor.jsx
+++ src/Pages/PopularInstructor/PopularInstructor.jsx
@@ -5,7 +5,7 @@ const PopularInstructor = () => {
     const [populars, setPopulars] = useState([])
 
     useEffect(() => {
-        fetch('popularInstructor.json')
+        fetch("http://localhost:5000/instructor")
             .then(res => res.json())
             .then(data => {
                 setPopulars(data)
@@ -13,14 +13,20 @@ const PopularInstructor = () => {
     }, [])
 
     return (
-        < div className='grid grid-cols-1 md:grid-cols-3 gap-10 mx-auto my-10'>
-            {
-                populars.slice(0, 6).map(popular =>
-                    <PopularInsCart
-                       key={popular._id}
-                       popular={popular}
-                    ></PopularInsCart>)
-            }
+        < div >
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-10 mx-auto my-10'>
+                {
+                    populars.slice(0, 6).map(popular =>
+                        <PopularInsCart
+                            key={popular._id}
+                            popular={popular}
+                        ></PopularInsCart>)
+                }
+            </div>
+
+            
+
+
         </div>
     );
 };
