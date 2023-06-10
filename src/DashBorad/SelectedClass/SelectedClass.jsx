0682d989bfcd8389d4e64b953bd7fe3d30386132
rhrubelr <rhrubel246@gmail.com>
2023-06-10 14:37:22 +0600

    added my selected class

--- src/DashBorad/SelectedClass/SelectedClass.jsx
+++ src/DashBorad/SelectedClass/SelectedClass.jsx
@@ -1,37 +1,76 @@
 import React from 'react';
 import useEnroll from '../../Hooks/useEnroll';
-import SelectedCard from './SelectedCard';
+import { Helmet } from 'react-helmet';
+import { Link } from 'react-router-dom';
+import { FaTrashAlt } from 'react-icons/fa';
 
 const SelectedClass = () => {
     const [cart] = useEnroll();
+
+    const total = cart.reduce((sum, item) => item.price + sum, 0)
+
 
     console.log(cart)
     return (
         <div>
 
-            <div className="overflow-x-auto">
-                <table className="table">
-                    {/* head */}
-                    <thead>
-                        <tr className="text-white text-xl">
-                            <th>#</th>
-                            <th>Photography Image</th>
-                            <th>Photography Name</th>
-                            <th>Price</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
+            <div className="w-full pb-[700px] pt-20  ">
+                <Helmet>
+                    <title>photography | Dashboard | my-Select-class</title>
+                </Helmet>
+                <div className="flex uppercase font-semibold justify-evenly items-center gap-4 mb-2">
+                    <h3 className="text-2xl">Total Class: <span className="text-red-500">{cart.length}</span></h3>
+                    <h3 className="text-2xl">Total Price: <span className="text-red-500">${total}</span></h3>
+                    <Link to="/dashboard/payment">
 
-                    {
-                        cart.map(card => <SelectedCard
-                            key={card._id}
-                            card={card}
-                        ></SelectedCard>)
-                    }
+                        <button className="btn border-0 btn-primary ">Pay Now</button>
+                    </Link>
+                </div>
 
-
-
-                </table>
+                <div data-aos="fade-up" className="overflow-x-auto mt-5  w-full">
+                    <table
+                        className="table w-full">
+                        {/* head */}
+                        <thead>
+                            <tr className=" text-xl">
+                                <th>Count</th>
+                                <th>Photography Image</th>
+                                <th>Photo Name</th>
+                                <th>Price</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {cart.map((item, index) => (
+                                <tr
+                                    key={item._id}>
+                                    <th>{index + 1}</th>
+                                    <td>
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img
+                                                    src={item.image}
+                                                    alt="Avatar Tailwind CSS Component"
+                                                />
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>{item.name}</td>
+                                    <td className="">{item.price}$
+                                    </td>
+                                    <th>
+                                        <button
+                                            onClick={() => handleDelete(item)}
+                                            className="btn bg-red-500 text-white border-0 "
+                                        >
+                                            <FaTrashAlt></FaTrashAlt>{" "}
+                                        </button>
+                                    </th>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     );
